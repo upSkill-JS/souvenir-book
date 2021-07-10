1242a2fdf1cc0db25884039c3f4127d05cee8b65
AMSKarmajeet <kcbhakta.99@gmail.com>
2021-07-10 18:32:28 +0530

    .env file gitignored

--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ app.use('/posts', postRoutes);
 
 // CONNECT to MongoDB Atlas Database
 
-// const CONNECTION_URL = 'mongodb+srv://amskarmajeet:amskarmajeet123@cluster0.rjsab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
